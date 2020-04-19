e082b50c3f0bb6e70ba0f054741cba08337a5a5b
Elijah Cobb <elijah@elijahcobb.com>
2020-04-19 17:35:34 -0400

    refactor

--- ts/LiCommandRegistry.ts
+++ ts/LiCommandRegistry.ts
@@ -6,23 +6,26 @@
  */
 import {LiBaseSocket} from "./LiBaseSocket";
 
-export type LiCommandRegistryCommand<P = any, R = any> = {
-	param: P;
-	return: R;
-};
-
-export type LiCommandRegistryStructure<T extends object = object> = {
-	[key in keyof T]: LiCommandRegistryCommand;
-};
-
+export type LiCommandRegistryCommand<P = any, R = any> = {param: P, return: R};
+export type LiCommandRegistryStructure<T extends object = object> = {[key in keyof T]: LiCommandRegistryCommand};
 export type LiCommandHandler = (value: any, socket: LiBaseSocket<any, any>) => Promise<any>;
-
 export type LiCommandName<T extends LiCommandRegistryStructure> = (keyof T) & string;
 export type LiCommand<T extends LiCommandRegistryStructure, C extends LiCommandName<T>> = T[C];
-export type LiCommandHandlerParam<T extends LiCommandRegistryStructure<T>, C extends LiCommandName<T>> = LiCommand<T, C>["param"];
-export type LiCommandHandlerReturn<T extends LiCommandRegistryStructure<T>, C extends LiCommandName<T>> = Promise<LiCommand<T, C>["return"]>;
 
-export type LiCommandHandlerStructure<LC extends LiCommandRegistryStructure<LC>, RC extends LiCommandRegistryStructure<RC>, C extends LiCommandName<LC>> = (value: LiCommandHandlerParam<LC, C>, socket: LiBaseSocket<RC, LC>) => LiCommandHandlerReturn<LC, C>;
+export type LiCommandHandlerParam<
+	T extends LiCommandRegistryStructure<T>,
+	C extends LiCommandName<T>
+> = LiCommand<T, C>["param"];
+
+export type LiCommandHandlerReturn<
+	T extends LiCommandRegistryStructure<T>,
+	C extends LiCommandName<T>
+> = Promise<LiCommand<T, C>["return"]>;
+
+export type LiCommandHandlerStructure<
+	LC extends LiCommandRegistryStructure<LC>,
+	RC extends LiCommandRegistryStructure<RC>, C extends LiCommandName<LC>
+> = (value: LiCommandHandlerParam<LC, C>, socket: LiBaseSocket<RC, LC>) => LiCommandHandlerReturn<LC, C>;
 
 export class LiCommandRegistry<T extends LiCommandRegistryStructure<T>> {
 
